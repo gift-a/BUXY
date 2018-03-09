769497606e66b30e0c63d18320ead9347d7f0ea9
gift-a <ksenia.gift@gmail.com>
2018-03-09 10:26:30 +0200

    file reading

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,7 +5,32 @@ export class AppComponent {
     this.mountPoint = mountPoint;
   }
 
+  handleFileImport(evt) {
+    console.log(this.fileInput.files[0]);
+    const fr = new FileReader();
+
+    fr.addEventListener("load", this.handleFileLoaded.bind(this));
+    fr.readAsText(this.fileInput.files[0]);
+  }
+
+  handleFileLoaded(info) {
+    console.log(info);
+    this.mountPoint.innerHTML += info.target.result
+      .toString()
+      .replace(/\n/g, "<br>");
+  }
+
+  querySelectors() {
+    this.fileInput = this.mountPoint.querySelector("#file");
+  }
+
+  addEventListeners() {
+    this.fileInput.addEventListener("change", this.handleFileImport.bind(this));
+  }
+
   mount() {
     this.mountPoint.innerHTML = template({ name: "Ihor" });
+    this.querySelectors();
+    this.addEventListeners();
   }
 }
